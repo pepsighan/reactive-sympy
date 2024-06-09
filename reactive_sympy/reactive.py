520767a53a0593bf1016d02d8504ffc56bfc4402
Sharad Chand <pepsighan@sharadchand.com>
2024-06-09 06:06:24 +0530

    When resolved do not proceed further

--- reactive_sympy/reactive.py
+++ reactive_sympy/reactive.py
@@ -3,7 +3,6 @@ import sympy
 
 class ReactiveSymbol(sympy.Symbol):
     _values: list[any]
-    _frozen: list[any]
 
     def __new__(cls, name, **assumptions):
         val = super().__new__(cls, name, **assumptions)
@@ -16,6 +15,9 @@ class ReactiveSymbol(sympy.Symbol):
 
         vals = [sympy.simplify(it) for it in v]
         self._values.append(vals)
+
+    def _is_resolved(self):
+        return any([all([is_known_value(v) for v in vals]) for vals in self._values])
 
 
 class ReactiveSympy:
@@ -34,14 +36,34 @@ class ReactiveSympy:
             return (symbs,)
 
     def eq(self, lhs: any, rhs: any) -> None:
+        self._internal_eq(lhs, rhs)
+        self.solve()
+
+    def _internal_eq(self, lhs: any, rhs: any) -> None:
         expr = sympy.Eq(lhs, rhs)
+
+        syms = expr.free_symbols
+        all_resolved = all([s._is_resolved() for s in syms])
+        if all_resolved:
+            return
+
         for sym in expr.free_symbols:
             solutions = sympy.solve(expr, sym)
             sym._add_values(solutions)
-        self.solve()
+
+        print({s.name: s._values for s in syms}, end="\n\n")
 
     def solve(self):
-        print("start solve")
+        print("solving...")
+        for symbol in self._all_symbols:
+            print("solving:", symbol)
+            for i in range(len(symbol._values)):
+                lhses = symbol._values[i]
+                for lhs in lhses:
+                    for j in range(i + 1, len(symbol._values)):
+                        rhses = symbol._values[j]
+                        for rhs in rhses:
+                            self._internal_eq(lhs, rhs)
 
 
 def is_known_value(v: any):
